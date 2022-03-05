7407080acedcfb553e26d88f8581b48055f86de4
dai-guu <89032169+dai-guu@users.noreply.github.com>
2022-03-05 23:43:51 +0900

    [Add]"5章"

---
+++ JavaWcp/src/Chapter4.java
@@ -0,0 +1,15 @@
+
+public class Chapter4 {
+
+	public static void main(String[] args) {
+		String name = "Hisato";
+		System.out.println(name);
+
+		name = "本音と建前";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
